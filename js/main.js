3785951dfd4ac405be54d43ac0b358cca31beb29
Jake <lejake888@gmail.com>
2020-10-05 23:20:45 +0100

    Player can input their name

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Pokemon Battle")
+let name = window.prompt("Enter your name: ")
+let playerParty = [];
+let rivalParty = [];
+
+// Setting up the "party" for each team
+
+console.log(playerParty)
+console.log(rivalParty)
+console.log(name)
+
+document.getElementById("name").innerHTML += name;
