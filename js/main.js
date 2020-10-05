0361514dcba2618d04793cc1f21ac8eb12546262
Jake <lejake888@gmail.com>
2020-10-05 23:28:31 +0100

    Player can pick pokemon

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 let name = window.prompt("Enter your name: ")
+document.getElementById("name").innerHTML += name;
 let playerParty = [];
 let rivalParty = [];
 
 // Setting up the "party" for each team
 
-console.log(playerParty)
-console.log(rivalParty)
-console.log(name)
+const pickPokemon = () => {
+    let pokemon1 = window.prompt("Pick your 1st Pokemon: ")
+    let pokemon2 = window.prompt("Pick your 2nd Pokemon ")
+    let pokemon3 = window.prompt("Pick your 3rd Pokemon: ")
+    
+    // Will have an image of pokemon, click to add to party
 
-document.getElementById("name").innerHTML += name;
+    console.log(pokemon1)
+    console.log(pokemon2)
+    console.log(pokemon3)
+}
+
+window.onload = () => {
+    pickPokemon()
+}
